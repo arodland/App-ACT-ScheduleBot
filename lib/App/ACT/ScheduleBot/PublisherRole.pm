17488c001182efd0bd45ea6bda9ddd4a2d274213
Andrew Rodland <andrew@cleverdomain.org>
2011-10-09 20:57:57 -0400

    Roles that want to provide additional poe_states have to blah blah blah

--- lib/App/ACT/ScheduleBot/PublisherRole.pm
+++ lib/App/ACT/ScheduleBot/PublisherRole.pm
@@ -2,9 +2,12 @@ package App::ACT::ScheduleBot::PublisherRole;
 use Moose::Role;
 with 'App::ACT::ScheduleBot::POERole';
 
-sub poe_states {
-  qw/announce_event/
-}
+sub poe_states { }
+
+around poe_states => sub {
+  my ($orig, $self) = @_;
+  return ($self->$orig(), 'announce_event');
+};
 
 requires 'announce_event';
 
